fc1b2d80a2a06ceda1e3e17c6c9b481667911ba8
CyberTycoon <silasokanla2006@gmail.com>
2025-02-11 06:31:03 -0600

    google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "../components/Navbar";
-import Head from "next/head";  // Import Head
 
 export const metadata: Metadata = {
   title: "Silas Okanlawon",
   description: "Welcome To My Portfolio, I am Silas Okanlawon, a versatile software developer with expertise in web development, data science, and Python automation. Proficient in full-stack development (React, Next.js, Python, Flask, Firebase, REST API) and creating dynamic user experiences. Skilled in data analysis, machine learning, task automation, and custom API integrations to deliver innovative solutions and streamline workflows. Passionate about solving complex problems and enhancing productivity with cutting-edge technology.",
+  verification: {
+    google: "7NZY7ue9nb7KjdiSNblV78TZRxZqlm0_zFGhVo409Ms",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -15,18 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        {/* Set your image as the favicon here */}
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <meta name="google-site-verification" content="7NZY7ue9nb7KjdiSNblV78TZRxZqlm0_zFGhVo409Ms" />
-      </Head>
-      <body
-        className={` antialiased`}
-      >
+      <body className="antialiased">
         <Navbar/>
         {children}
       </body>
     </html>
   );
 }
-
